6cce37f01d7dfd127ddc2a10142b02dc73a974f9
THIERRY Landry <thierry.iwikotan@digipay-services.com>
2026-03-29 19:46:00 +0100

    correction de fichier postcss.config.cjs

--- Front/postcss.config.cjs
+++ Front/postcss.config.cjs
@@ -1,12 +1,7 @@
 // postcss.config.cjs
 module.exports = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-    autoprefixer: {},
-  },
-};
   plugins: [
-    require('tailwindcss'),  // Ajoutez Tailwind CSS
-    require('autoprefixer'),  // Ajoutez Autoprefixer pour la compatibilité des navigateurs
+    require('tailwindcss'),
+    require('autoprefixer'),
   ],
 };
